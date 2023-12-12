46b680b31e578d028ee309c75aae6b11e7fc9dce
PG Softcode <pg.softcode@gmail.com>
2023-12-12 11:51:44 +0530

    Initial Commit

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+    server: {
+        host: '192.168.1.183'
+        // host: '192.168.1.10'
+    },
+});
